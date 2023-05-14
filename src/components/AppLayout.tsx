49aa17ce0eb611d0da9d6b16715a7368ccaa94b6
Erwin Villejo <erwinvillejo@gmail.com>
2023-05-14 13:25:42 +0800

    add ycombinator logo and favicon

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,41 @@
-import { Box, Container, Tab, TabList, Tabs } from '@mui/joy'
+import {
+  AspectRatio,
+  Box,
+  Container,
+  IconButton,
+  Link,
+  Tab,
+  TabList,
+  Tabs,
+  Tooltip,
+} from '@mui/joy'
 import { PropsWithChildren, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import yCombinatorLogo from '~/assets/ycombinator.svg'
+
+const logo = (
+  <Tooltip title="Hacker News icon by Icons8">
+    <IconButton
+      variant="plain"
+      component={Link}
+      href="https://icons8.com/icon/16336/hacker-news"
+      target="_blank"
+      rel="noopenner"
+      sx={(theme) => ({
+        '&:hover': {
+          bgcolor: 'transparent',
+        },
+        [`${theme.getColorSchemeSelector('dark')} img`]: {
+          filter: 'invert(1)',
+        },
+      })}
+    >
+      <AspectRatio ratio="1" variant="plain" sx={{ minWidth: 48 }}>
+        <img src={yCombinatorLogo} />
+      </AspectRatio>
+    </IconButton>
+  </Tooltip>
+)
 
 interface AppLayoutProps {
   navPages: [label: string, path: string][]
@@ -33,19 +68,23 @@ export default function AppLayout({
           },
         }}
       >
-        <Tabs value={currentPath} onChange={(_, value) => navigate(`${value}`)}>
-          <TabList variant="outlined" sx={{ mt: 1 }}>
-            {navPages.map(([label, path]) => (
-              <Tab
-                key={label}
-                variant={currentPath.startsWith(path) ? 'solid' : 'plain'}
-                value={path}
-              >
-                {label}
-              </Tab>
-            ))}
-          </TabList>
-        </Tabs>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'auto 1fr', alignItems: 'center', mt: 1 }}>
+          {logo}
+          <Tabs value={currentPath} onChange={(_, value) => navigate(`${value}`)}>
+            <TabList variant="plain" sx={{ width: '100%' }}>
+              {navPages.map(([label, path]) => (
+                <Tab
+                  key={label}
+                  variant={currentPath.startsWith(path) ? 'solid' : 'plain'}
+                  value={path}
+                >
+                  {label}
+                </Tab>
+              ))}
+            </TabList>
+          </Tabs>
+        </Box>
+
         <main>{children}</main>
       </Box>
     </Container>
